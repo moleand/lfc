ee76dc2a3be326b2642143efd9234f42f1d9d6d2
Alex Vakhtin <vkstrfrt@gmail.com>
2019-03-03 13:31:15 +0300

    geolocation

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+new Vue({
+    el: '#app',
+    data: {
+        region: '',
+        city: ''
+    },
+    mounted: function () {
+        axios.get(' http://api.sypexgeo.net/json/')
+            .then(res => {
+                console.log(res);
+                this.region = res.data.region.name_ru;
+                this.city = res.data.city.name_ru;
+            })
+    }
 });
